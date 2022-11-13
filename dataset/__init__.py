18da07da4e82207c0370c97837fac07caf8e377b
davidpengucf <102485354+davidpengucf@users.noreply.github.com>
2022-11-12 21:15:49 -0500

    Add files via upload

---
+++ dataset/__init__.py
@@ -0,0 +1,13 @@
+from .rendered_hand_pose import RenderedHandPose
+from .hand_3d_studio import Hand3DStudio, Hand3DStudioAll
+from .freihand import FreiHand
+
+from .surreal import SURREAL
+from .lsp import LSP
+from .human36m import Human36M
+
+from .synanimal_horse import SynAnimalHorse
+from .synanimal_tiger import SynAnimalTiger
+from .tigdog_horse import TigDogHorse
+from .tigdog_tiger import TigDogTiger
+
