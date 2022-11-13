b1aeb820db330f0e7690cc2d50107081c445e9f9
davidpengucf <102485354+davidpengucf@users.noreply.github.com>
2022-11-12 22:09:59 -0500

    Update __init__.py

--- dataset/__init__.py
+++ dataset/__init__.py
@@ -6,8 +6,5 @@ from .surreal import SURREAL
 from .lsp import LSP
 from .human36m import Human36M
 
-from .synanimal_horse import SynAnimalHorse
-from .synanimal_tiger import SynAnimalTiger
-from .tigdog_horse import TigDogHorse
-from .tigdog_tiger import TigDogTiger
+
 
